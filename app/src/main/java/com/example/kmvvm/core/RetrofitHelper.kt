e7903869a117c07e3aac11d4d3af343dd7bf4e24
MJackson22-bit <jm5444859@gmail.com>
2022-03-24 17:51:05 -0600

    Configurando Dagger Hilt
    Agregando dependencia, plugin, compilador

--- app/src/main/java/com/example/kmvvm/core/RetrofitHelper.kt
+++ app/src/main/java/com/example/kmvvm/core/RetrofitHelper.kt
@@ -3,12 +3,10 @@ package com.example.kmvvm.core
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitHelper {
-    companion object {
-        fun getRetrofit(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl("https://drawsomething-59328-default-rtdb.europe-west1.firebasedatabase.app/")
-                .addConverterFactory(GsonConverterFactory.create()).build()
-        }
+object RetrofitHelper {
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://drawsomething-59328-default-rtdb.europe-west1.firebasedatabase.app/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
     }
 }
